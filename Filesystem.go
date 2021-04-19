87c1b58cb2bc15cf62c569d80ec5088988a35766
fangjw <fangjw@wecut.com>
2021-04-19 16:50:01 +0800

    增加创建文件夹方法

--- Filesystem.go
+++ Filesystem.go
@@ -1,1 +1,34 @@
 package filesystem
+
+import "os"
+
+func Mkdir(path string,  mode os.FileMode){
+
+	if path == ""{
+		return
+	}
+
+	err := os.MkdirAll(path, mode)
+
+	if err != nil{
+		panic(err)
+	}
+}
+
+func MultiMkdir(paths []string,  mode os.FileMode){
+
+	if len(paths) == 0{
+		return
+	}
+
+	for _,path := range paths{
+
+		err := os.MkdirAll(path, mode)
+
+		if err != nil{
+			panic(err)
+		}
+	}
+
+
+}
